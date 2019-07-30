82dd8bfce1a835efe476ee56400f22c842daffd0
shenzongyi1997 <858843614@qq.com>
2019-07-30 21:23:01 +0800

    Add files via upload

--- decay_period.m
+++ decay_period.m
@@ -6,9 +6,10 @@ if nargin == 1
     num_of_T = 100;
 end
 result = zeros(1, length(signal).*num_of_T);
-for i = 1:num_of_T
+result(1:length(signal)) = signal
+for i = 2:num_of_T
     %length(signal)
-    result((i-1)*length(signal) + 1: i*length(signal)) = signal.*alpha;
+    result((i-1)*length(signal) + 1: i*length(signal)) = (result((i-2)*length(signal) + 1: (i - 1)*length(signal))).*alpha;
 infin_per_signal = result;
 
     
