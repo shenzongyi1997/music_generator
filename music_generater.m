f37594666401c3cc3068d78713674c72b42c4507
shenzongyi1997 <858843614@qq.com>
2019-07-30 21:33:43 +0800

    Add files via upload

--- music_generater.m
+++ music_generater.m
@@ -8,7 +8,7 @@
 % for i = length(signal)/2 + 1 : length(signal)
 %     signal(i) = 0 - signal(i - length(signal)/2);
 % end 
-% fs = 48 * 1000;
+
 
 % 随机
 % signal = zeros(1,1000);
@@ -18,22 +18,37 @@
 
 
 %鸟叫
+% signal = zeros(1,1000);
+% f = 0.2
+% for i = 1:length(signal)
+%     signal(i) = sin(f*i) * (1/length(signal)) * i;
+% end
+
+
+%白噪音
+% signal = zeros(1, 100);
+% for i = 1:length(signal)
+%     signal(i) = 0.5;
+% end
+
+%一上一下
 signal = zeros(1,100);
-f = 0.8
 for i = 1:length(signal)
-    signal(i) = sin(f*i) * (1/length(signal)) * i;
+    signal(i) = mod(i,2)*(-2) + 1;
 end
 
 
+ fs = 48 * 100;
 %以下是可视化和生成音乐的
 figure
 plot(signal)
 for i = 1:length(signal)
     line([i  i], [0  signal(i)])
 end
+ylim([-1 1])
 title("原始信号")
 
-music = decay_period(0.6, 10, signal);
+music = decay_period(0.6, 60, signal);
 figure
 plot(music)
 for i = 1:length(music)
